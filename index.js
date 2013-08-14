cbc7e5378c533f995ec94715223eb9bf639d316f
Dan Crews <crewsd@gmail.com>
2013-08-14 15:26:36 -0600

    Adding the endpoint to hit for restarting

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 var express = require('express')
+  , heroku = require('heroku.js')
   , app = module.exports = express();
+
+app.post('/restart/:appName', function(req, res, next) {
+  var appName = req.params.appName
+    , privateKey = req.headers['x-user-key'];
+
+  if (privateKey !== process.env.PRIVATE_KEY) return res.send(401);
+
+  new heroku.App(appName).process().restart(function(success) {
+    res.send(success ? 200 : 400);
+  });
+});
---
+++ sample.js
@@ -0,0 +1,40 @@
+#!/Users/dncrews/nvm/v0.8.25/bin/node
+
+var heroku = require('heroku.js');
+
+// heroku.account.get(function(account) {
+//   console.log("Account: ", account);
+// });
+
+// console.log(heroku.app('pickmyplate'));
+
+// heroku.app.getApps(function(apps) {
+//   var app, i, l;
+//   for (i=0, l=apps.length; i<l; i++) {
+//     app = apps[i];
+//     if (app.name === 'pickmyplate') {
+//       app.process().restart(function() {
+//         console.log(arguments);
+//       });
+//       break;
+//     }
+//   }
+// });
+
+
+// heroku.app('pickmyplate').process.restart(function() {
+//   console.log(arguments);
+// });
+
+
+// heroku.account.patch({allow_tracking: true}, function(changed) {
+//   console.log("Changed: ", changed);
+// });
+
+// heroku.account.password(process.env.HEROKU_NEW_PASSWORD, process.env.HEROKU_PASSWORD, function() {
+//   console.log(arguments);
+// });
+
+
+
+
